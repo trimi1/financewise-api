ac6566fa39b58e38bd62dce535cd83a9951195e1
Q210061 <a.maghe@student.helmo.be>
2024-10-20 23:56:20 +0200

    Adding CORS security

--- api/src/main/java/be/helmo/api/security/ApplicationConfig.java
+++ api/src/main/java/be/helmo/api/security/ApplicationConfig.java
@@ -1,14 +1,12 @@
 package be.helmo.api.security;
 
 import be.helmo.api.infrastructure.repository.IUserRepository;
-import be.helmo.api.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,7 +20,6 @@ public class ApplicationConfig {
     public ApplicationConfig(IUserRepository userRepository) {
         this.userRepository = userRepository;
     }
-
 
     @Bean
     UserDetailsService userDetailsService() {
